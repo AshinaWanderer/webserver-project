957471e81f42d2060e486d347d97305462992f72
jack <85391755+AshinaWanderer@users.noreply.github.com>
2026-03-11 17:00:46 +0100

    Updated start.sh to set keepalive later

--- ha-nginx-ha/proxy/start.sh
+++ ha-nginx-ha/proxy/start.sh
@@ -1,19 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-echo "Starting Keepalived..."
-keepalived --dont-fork --log-console --log-detail &
-KEEPALIVED_PID=$!
-
-# Kurz warten damit VIP zugewiesen wird
-sleep 2
-
-echo "Starting Nginx..."
-nginx -g 'daemon off;' &
-NGINX_PID=$!
-
-# Warte auf beide Prozesse - wenn einer stirbt, beende alles
-wait -n $KEEPALIVED_PID $NGINX_PID
-echo "One process exited, shutting down..."
-kill $KEEPALIVED_PID $NGINX_PID 2>/dev/null
-exit 1
+exec keepalived -p /var/run/keepalived.pid -P -f /etc/keepalived/keepalived.conf
